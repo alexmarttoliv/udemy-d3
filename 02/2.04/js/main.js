3690f66cd985e1a20cdcafef762fa9bd778b4d7c
Alexandre Oliveira <alexmartt@outlook.com>
2026-01-14 00:18:40 -0300

    practice D3 updates

--- 02/2.04/js/main.js
+++ 02/2.04/js/main.js
@@ -9,7 +9,27 @@ const svg = d3.select("#chart-area").append("svg")
   .attr("height", 400)
 
 svg.append("circle")
+  .attr("cx", 200)
+  .attr("cy", 200)
+  .attr("r", 100)
+  .attr("fill", "blue")
+
+svg.append("rect")
+  .attr("x", 100)
+  .attr("y", 300)
+  .attr("width", 200)
+  .attr("height", 80)
+  .attr("fill", "green")
+
+
+/*
+const svg = d3.select("#chart-area").append("svg")
+  .attr("width", 400)
+  .attr("height", 400)
+
+svg.append("circle")
   .attr("cx", 100)
   .attr("cy", 250)
   .attr("r", 70)
   .attr("fill", "red")
+  */
--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,4 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
